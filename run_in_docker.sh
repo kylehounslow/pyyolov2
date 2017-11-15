0f0b109ea5ccacb2809a593c3c0b5cf7e16d3d7c
Kyle Hounslow <kyle.hounslow@gmail.com>
2017-11-15 11:26:59 -0800

    fix demo text

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-GPU_DEVICE=$2
 CAM_INDEX=$1
 xhost +local:
-nvidia-docker run -it --rm --device="/dev/video0" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY kylehounslow/pyyolov2:latest /usr/bin/python /home/pyyolov2/python/pyyolov2.py 0 0
+nvidia-docker run -it --rm --device="/dev/video0" --device="/dev/video1" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY kylehounslow/pyyolov2:latest /bin/bash -c "git pull origin master; /usr/bin/python /home/pyyolov2/python/pyyolov2.py 0 $CAM_INDEX"
+
 
